7a499c3ea5a2e9d48769db3b36afb60f186c27fb
BLUE <hoanghieu25102@gmail.com>
2023-07-15 01:35:59 +0700

    remove dotenv socket.mjs

--- src/socket.mjs
+++ src/socket.mjs
@@ -3,124 +3,110 @@ import Peer from "peerjs";
 import playVideo from "./services/playVideo.mjs";
 import io from "socket.io-client";
 
-// Define the environment variables
-const { SOCKET_SERVER_URL } = process.env;
+const socket = io("https://blue-sn.onrender.com");
 
-// Check if the SOCKET_SERVER_URL is defined
-if (!SOCKET_SERVER_URL) {
-  console.error("Missing SOCKET_SERVER_URL environment variable.");
-  // Handle the error accordingly, such as showing an error message or falling back to a default URL.
-  alert("Missing environment variable: SOCKET_SERVER_URL");
-}
+socket.on("connect_error", (error) => {
+  console.error("Lỗi kết nối Socket.io:", error);
+  alert("Lỗi kết nối đến máy chủ Socket.io");
+});
 
-// Continue with the code only if the SOCKET_SERVER_URL is defined
-if (SOCKET_SERVER_URL) {
-  const socket = io(SOCKET_SERVER_URL);
+socket.on("connect_timeout", () => {
+  console.error("Hết thời gian kết nối Socket.io");
+  alert("Hết thời gian kết nối đến máy chủ Socket.io");
+});
 
-  socket.on("connect_error", (error) => {
-    console.error("Lỗi kết nối Socket.io:", error);
-    alert("Lỗi kết nối đến máy chủ Socket.io");
-  });
+socket.on("error", (error) => {
+  console.error("Lỗi Socket.io:", error);
+  alert("Lỗi kết nối đến máy chủ Socket.io");
+});
 
-  socket.on("connect_timeout", () => {
-    console.error("Hết thời gian kết nối Socket.io");
-    alert("Hết thời gian kết nối đến máy chủ Socket.io");
-  });
-
-  socket.on("error", (error) => {
-    console.error("Lỗi Socket.io:", error);
-    alert("Lỗi kết nối đến máy chủ Socket.io");
-  });
-
-  socket.on("server-send-success", (data) => {
-    document.getElementById("login").style.display = "none";
-    document.getElementById("chat").style.display = "block";
-    console.log("", data);
-    const peerID = data.username;
-    const peer = new Peer(peerID);
-    socket.emit("new-peerId", peerID);
-    document
-      .getElementById("list-user")
-      .addEventListener("click", function (event) {
-        const peerId = event.target.textContent;
-        openCamera((stream) => {
-          playVideo(stream, "localVideo");
-          const call = peer.call(peerId, stream);
-          call.on("stream", (remoteStream) => {
-            playVideo(remoteStream, "friendVideo");
-          });
-        });
-      });
-
-    peer.on("call", (call) => {
+socket.on("server-send-success", (data) => {
+  document.getElementById("login").style.display = "none";
+  document.getElementById("chat").style.display = "block";
+  console.log("", data);
+  const peerID = data.username;
+  const peer = new Peer(peerID);
+  socket.emit("new-peerId", peerID);
+  document
+    .getElementById("list-user")
+    .addEventListener("click", function (event) {
+      const peerId = event.target.textContent;
       openCamera((stream) => {
         playVideo(stream, "localVideo");
-        call.answer(stream);
+        const call = peer.call(peerId, stream);
         call.on("stream", (remoteStream) => {
           playVideo(remoteStream, "friendVideo");
         });
       });
     });
 
-    socket.on("users-online", (userList) => {
-      console.log(userList);
-      let listUser = document.getElementById("list-user");
-      listUser.innerHTML = "";
-      userList.forEach((element) => {
+  peer.on("call", (call) => {
+    openCamera((stream) => {
+      playVideo(stream, "localVideo");
+      call.answer(stream);
+      call.on("stream", (remoteStream) => {
+        playVideo(remoteStream, "friendVideo");
+      });
+    });
+  });
+  socket.on("users-online", (userList) => {
+    console.log(userList);
+    let listUser = document.getElementById("list-user");
+    listUser.innerHTML = "";
+    userList.forEach((element) => {
+      let li = document.createElement("li");
+      li.textContent = element;
+      li.id = element;
+      listUser.appendChild(li);
+    });
+  });
+
+  socket.on("new-user-connect", (userList) => {
+    console.log(userList);
+
+    let listUser = document.getElementById("list-user");
+    userList.forEach((element) => {
+      if (!document.getElementById(element)) {
         let li = document.createElement("li");
         li.textContent = element;
         li.id = element;
+
         listUser.appendChild(li);
-      });
-    });
-
-    socket.on("new-user-connect", (userList) => {
-      console.log(userList);
-
-      let listUser = document.getElementById("list-user");
-      userList.forEach((element) => {
-        if (!document.getElementById(element)) {
-          let li = document.createElement("li");
-          li.textContent = element;
-          li.id = element;
-
-          listUser.appendChild(li);
-        }
-      });
-    });
-
-    socket.on("user-disconnect", (peerId) => {
-      const element = document.getElementById(peerId);
-      if (element) {
-        element.parentNode.removeChild(element);
       }
     });
   });
 
-  socket.on("server-send-regist-success", (data) => {
-    alert("Regist success!");
-
-    document.getElementById("txtName").value = "";
-    document.getElementById("txtPass").value = "";
-    document.getElementById("txtVerifyPass").value = "";
+  socket.on("user-disconnect", (peerId) => {
+    const element = document.getElementById(peerId);
+    if (element) {
+      element.parentNode.removeChild(element);
+    }
   });
+});
 
-  socket.on("server-send-fail", () => {
-    alert("User has existed");
-  });
+socket.on("server-send-regist-success", (data) => {
+  alert("Regist success!");
 
-  socket.on("server-login-fail", () => {
-    alert("User is not existed");
-  });
+  document.getElementById("txtName").value = "";
+  document.getElementById("txtPass").value = "";
+  document.getElementById("txtVerifyPass").value = "";
+});
 
-  socket.on("server-login-fail-pass", () => {
-    alert("Wrong password!");
-  });
+socket.on("server-send-fail", () => {
+  alert("User has existed");
+});
 
-  socket.on("server-send-fail-pass", () => {
-    alert("Password is not match");
-  });
-}
+socket.on("server-login-fail", () => {
+  alert("User is not existed");
+});
+
+socket.on("server-login-fail-pass", () => {
+  alert("Wrong password!");
+});
+
+socket.on("server-send-fail-pass", () => {
+  alert("Password is not match");
+});
 
 document.addEventListener("DOMContentLoaded", () => {
   document.getElementById("login").style.display = "block";
