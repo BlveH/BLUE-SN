b1bf25d5c3d18195e731ed886b3d323c223dd17b
BLUE <hoanghieu25102@gmail.com>
2023-07-14 15:02:04 +0700

    connectDB v2

--- src/configs/connectDB.mjs
+++ src/configs/connectDB.mjs
@@ -2,7 +2,10 @@ import mongoose from "mongoose";
 
 export const connect = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("DB connect successful");
   } catch (error) {
     console.error("Failed to connect to MongoDB:", error.message);
