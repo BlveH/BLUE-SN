b6c605c8a4eb0e6cc21af20909a5c9b356106ade
BLUE <hoanghieu25102@gmail.com>
2023-07-14 15:09:29 +0700

    connectDB v3

--- src/configs/connectDB.mjs
+++ src/configs/connectDB.mjs
@@ -5,6 +5,7 @@ export const connect = async () => {
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 5000,
     });
     console.log("DB connect successful");
   } catch (error) {
